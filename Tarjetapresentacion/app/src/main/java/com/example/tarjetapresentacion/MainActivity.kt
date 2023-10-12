89fe18d73716768e0ee24ca5689efc0d769956bf
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2023-10-12 17:39:52 +0100

    Tarjeta de presentacion

---
+++ Tarjetapresentacion/app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.example.tarjetapresentacion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TarjetaPresentacionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.LightGray
+                ) {
+                    Presentation("Andres Felipe Vargas", "Android developer")
+                    Info("555-555-55","Andrew02","Andrew@gmail.com")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Presentation(name: String,ocupation: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_launcher_foreground)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = name,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(5.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = ocupation,
+            fontSize = 10.sp,
+            color = Color.Green,
+            modifier = Modifier
+                .padding(5.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun Info(phone: String, social: String, email: String,  modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.ic_launcher_foreground)
+    val imageModifier = Modifier
+        .size(35.dp)
+        .border(BorderStroke(1.dp, Color.Black))
+        .background(Color.Yellow)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Row(
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = imageModifier
+            )
+
+            Text(
+                text = phone,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+        }
+        Row(
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = imageModifier
+            )
+
+            Text(
+                text = "@" + social,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+        }
+        Row(
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = imageModifier
+            )
+
+            Text(
+                text = email,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TarjetaPresentacionTheme {
+        Presentation("Andres Felipe Vargas", "Android developer")
+        Info("555-555-55","Andrew02","Andrew@gmail.com")
+    }
+}
